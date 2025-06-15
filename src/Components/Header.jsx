ff9d47c10fd81a39f9e21e860a7a0c15997e1069
sinclaire-white <shahriyarsifat2@gmail.com>
2025-06-15 09:34:17 +0600

    basic stricture of navbar creater

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import { NavLink } from "react-router";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>This is Header</h1>
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+        {/* logo & name */}
+        <div className="navbar-start gap-2">
+            <div>
+                <img src={'https://i.ibb.co/QF2hrC2b/Civitas-logo-2.png'} alt="" className="h-20 w-20"/>
+            </div>
+          <p className="text-5xl">Civitas</p>
         </div>
-    );
+        {/* upcoming events */}
+        <div className="navbar-center hidden lg:flex">
+          <NavLink to={'/upcoming-events'}><a className=" text-xl">Upcoming Events</a></NavLink>
+        </div>
+        {/* avatan and others */}
+        <div className="navbar-end">
+            <div className="flex gap-2">
+                <NavLink to={'/register'}><button className="btn">Register</button></NavLink>
+                <NavLink to={'/login'}><button className="btn">Login</button></NavLink>
+            </div>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <NavLink to={'/create-events'}>Create Event</NavLink>
+              </li>
+              <li>
+                <NavLink to={'/manage-events'}>Manage Events</NavLink>
+              </li>
+              <li>
+                <NavLink to={'/join-events'}>Join Events</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,67 +1,69 @@
 import { NavLink } from "react-router";
 
-
 const Register = () => {
+  // onSubmit={handleSignUp}
+  // onClick={handleGoogleSignUp}
+  // {passwordError && <p className="text-red-500">{passwordError}</p>}
 
+  return (
+    <div className="h-screen mt-40">
+      <form>
+        <fieldset className="p-4 mx-auto border fieldset bg-secondary border-base-300 rounded-box w-2/4">
+          <h2 className="mb-5 text-5xl font-semibold text-center">
+            Register to Civitas
+          </h2>
+          <label className="font-bold label">Name</label>
+          <input
+            type="text"
+            className="input w-full"
+            placeholder="Name"
+            name="name"
+            required
+          />
+          <label className="font-bold label">Photo URL</label>
+          <input
+            type="text"
+            className="input w-full"
+            placeholder="Photo URL"
+            name="photo"
+          />
+          <label className="font-bold label">Email</label>
+          <input
+            type="email"
+            className="input w-full"
+            placeholder="Email"
+            name="email"
+            required
+          />
+          <label className="font-bold label">Password</label>
+          <input
+            type="password"
+            className="input w-full"
+            placeholder="Password"
+            name="password"
+            required
+          />
 
-
-// onSubmit={handleSignUp}
-// onClick={handleGoogleSignUp}
-// {passwordError && <p className="text-red-500">{passwordError}</p>}
-
-
-    return (
-        <div className="h-screen mt-40">
-            <form>
-          <fieldset className="p-4 mx-auto border fieldset bg-secondary border-base-300 rounded-box w-xs">
-            <h2 className="mb-5 text-5xl font-semibold text-center">Sign Up</h2>
-            <label className="font-bold label">Name</label>
-            <input type="text" className="input" placeholder="Name" name="name" required />
-            <label className="font-bold label">Photo URL</label>
-            <input
-              type="text"
-              className="input"
-              placeholder="Photo URL"
-              name="photo"
-            />
-            <label className="font-bold label">Email</label>
-            <input
-              type="email"
-              className="input"
-              placeholder="Email"
-              name="email"
-              required
-            />
-            <label className="font-bold label">Password</label>
-            <input
-              type="password"
-              className="input"
-              placeholder="Password"
-              name="password"
-              required
-            />
-            
-            <button className="mt-4 btn btn-neutral text-secondary hover:bg-base-100">
-              Sign Up
-            </button>
-              <div className="divider divider-neutral">Or</div>
-            <button
-              type="button"
-              className="bg-black btn text-secondary btn-neutral hover:bg-base-100"
-              
-            >
-             Register with Google
-            </button>
-            <p className="mt-2 ml-2">
-              Already registered?
-              <NavLink to={"/login"} className={"underline ml-1"}>
-                Login Now
-              </NavLink>
-            </p>
-          </fieldset>
-        </form>
-        </div>
-    );
+          <button className="mt-4 btn btn-neutral text-secondary hover:bg-base-100">
+            Sign Up
+          </button>
+          <div className="divider divider-neutral">Or</div>
+          <button
+            type="button"
+            className="bg-black btn text-secondary btn-neutral hover:bg-base-100"
+          >
+            Register with Google
+          </button>
+          <p className="mt-2 ml-2">
+            Already registered?
+            <NavLink to={"/login"} className={"underline ml-1"}>
+              Login Now
+            </NavLink>
+          </p>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
