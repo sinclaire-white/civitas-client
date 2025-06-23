bb39490c8057f467d4aa88930b69094829d874a8
sinclaire-white <shahriyarsifat2@gmail.com>
2025-06-23 15:43:21 +0600

    minor change

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -12,7 +12,7 @@ const PrivateRoute = ({ children }) => {
     }
 
     if(!user){
-       return <Navigate to="/register" state={{ from: location }} replace />;
+       return <Navigate to="/login" state={{ from: location }} replace />;
     }
 
     return children;
